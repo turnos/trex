fbe8f599d4b28c27219d8e92ab7d9f41dcfcf259
Turnos <164613863+turnos@users.noreply.github.com>
2025-01-06 14:56:34 +0100

    Add support for colima agent

--- app/trakt.py
+++ app/trakt.py
@@ -31,40 +31,51 @@ def create_scrobble_object(plex_payload):
             logger.debug("movie ids: %s", ids)
             
     elif metadata["type"] == "episode":
-        show = result["show"] = {}
+        if "grandparentGuid" in metadata:
+            # Colima allows us to organize movies as episodes for mixed libraries, so we need to check if the media is actually managed by colima agent which uses tmdb ids
+            grandparentGuid = metadata["grandparentGuid"]
+            colima_id = search_colima_id(grandparentGuid)
+            if colima_id:
+                movie = result["movie"] = {}
+                movie["title"] = metadata["title"]
+                movie["year"] = metadata["year"]
+                ids = movie["ids"] = {}
+                ids["tmdb"] = colima_id
+            else:
+                show = result["show"] = {}
 
-        if "grandparentSlug" in metadata:
-            show_ids = show["ids"] = {}
-            show_ids["slug"] = metadata["grandparentSlug"]
+                if "grandparentSlug" in metadata:
+                    show_ids = show["ids"] = {}
+                    show_ids["slug"] = metadata["grandparentSlug"]
         
-        show["title"] = metadata["grandparentTitle"]
+                show["title"] = metadata["grandparentTitle"]
 
-        episode = result["episode"] = {}
-        episode["title"] = metadata["title"]
-        episode["season"] = metadata["parentIndex"]
-        episode["number"] = metadata["index"]
+                episode = result["episode"] = {}
+                episode["title"] = metadata["title"]
+                episode["season"] = metadata["parentIndex"]
+                episode["number"] = metadata["index"]
         
-        if "Guid" in metadata:
+                if "Guid" in metadata:
             
-            ids = episode["ids"] = {}
+                    ids = episode["ids"] = {}
         
-            guids = str(metadata["Guid"])
+                    guids = str(metadata["Guid"])
         
-            logger.debug("Guids in payload:  %s", guids)
+                    logger.debug("Guids in payload:  %s", guids)
         
-            tvdb_id = search_tvdb_id(guids)
-            if tvdb_id:
-                ids["tvdb"] = tvdb_id
+                    tvdb_id = search_tvdb_id(guids)
+                    if tvdb_id:
+                        ids["tvdb"] = tvdb_id
         
-            imdb_id = search_imdb_id(guids)      
-            if imdb_id:
-                ids["imdb"] = imdb_id
+                    imdb_id = search_imdb_id(guids)      
+                    if imdb_id:
+                        ids["imdb"] = imdb_id
             
-            tmdb_id = search_tmdb_id(guids)
-            if tmdb_id:
-                ids["tmdb"] = tmdb_id
+                    tmdb_id = search_tmdb_id(guids)
+                    if tmdb_id:
+                        ids["tmdb"] = tmdb_id
         
-            logger.debug("episode ids: %s", ids)
+                    logger.debug("episode ids: %s", ids)
         
     return result or None
 
@@ -86,5 +97,12 @@ def search_tvdb_id(guid_str:str):
     tvdb_match = re.search(r"tvdb://(?P<tvdb_id>\d+)", guid_str)
     if tvdb_match:
         return int(tvdb_match.group("tvdb_id"))
+    else:
+        return None
+    
+def search_colima_id(guid_str:str):
+    colima_match = re.search(r"com.plexapp.agents.colima://(?P<colima_id>\d+)?", guid_str)
+    if colima_match:
+        return int(colima_match.group("colima_id"))
     else:
         return None
